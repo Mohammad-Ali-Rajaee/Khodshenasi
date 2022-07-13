8989fd2c00b8cc9637fae625b33410f8498f6bd0
ali <m.a.rajaee115369@gmail.com>
2022-07-13 08:21:58 -0700

    updated: back to top button fixed

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
 const navslide = () => {
-    const menu = document.querySelector('.menu');
-    const header = document.querySelector('.else');
-    const menu_btn = document.querySelector('.hambergur');
+  const menu = document.querySelector(".menu");
+  const header = document.querySelector(".else");
+  const menu_btn = document.querySelector(".hambergur");
 
-    menu.addEventListener('click', () => {
-        header.classList.toggle('else-active');
+  menu.addEventListener("click", () => {
+    header.classList.toggle("else-active");
 
-        menu_btn.classList.toggle('is-active');
-    });
+    menu_btn.classList.toggle("is-active");
+  });
+};
+/* back to top button */
+const BackToTopBtn = document.querySelector(".top");
+const FixedHeader = document.querySelector(".main-header");
+window.addEventListener("scroll", ToTop);
 
+function ToTop() {
+  if (window.pageYOffset > 100) {
+    if (!BackToTopBtn.classList.contains("Back_To_Top")) {
+        BackToTopBtn.classList.remove("Back_To_Top_End");
+        BackToTopBtn.classList.add("Back_To_Top");
+        BackToTopBtn.style.display = "block";
+        // FixedHeader.classList.add("fixed-header");
+    }
+  } else {
+    BackToTopBtn.style.display = "none";
+    if (BackToTopBtn.classList.contains("Back_To_Top")) {
+        BackToTopBtn.classList.remove("Back_To_Top");
+        BackToTopBtn.classList.add("Back_To_Top_End");
+    }
+}
+}
+BackToTopBtn.addEventListener("click", back);
 
+function back() {
+  window.scrollTo(0, 0);
 }
 
 const dropdownLanguages = () => {
-    const drop = document.querySelector('.dropdown');
+  const drop = document.querySelector(".dropdown");
 
-    menu.addEventListener('click', () => {
-        drop.classList.add('languages');
-
-    });
-
-
-}
+  menu.addEventListener("click", () => {
+    drop.classList.add("languages");
+  });
+};
 navslide();
