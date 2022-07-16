0d1136579fc1500c93f0ea9afe199d79e8c4e1ce
ali <m.a.rajaee115369@gmail.com>
2022-07-15 22:25:25 -0700

    updated: dropdown got updated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 //------------hamburger menu-------------------------
 
 const navslide = () => {
@@ -7,13 +6,13 @@ const navslide = () => {
   const menu_btn = document.querySelector(".hambergur");
 
   menu.addEventListener("click", menu_open);
-  
-  function menu_open(event){
-  event.stopPropagation();
+
+  function menu_open(event) {
+    event.stopPropagation();
     header.classList.toggle("else-active");
 
     menu_btn.classList.toggle("is-active");
-  };
+  }
 
   document.addEventListener("click", menu_close);
   function menu_close() {
@@ -22,7 +21,6 @@ const navslide = () => {
       menu_btn.classList.remove("is-active");
     }
   }
-
 };
 
 navslide();
@@ -46,23 +44,53 @@ function back() {
   window.scrollTo(0, 0);
 }
 
-
-
 //-------dropdown----------------------------------------------
 
-const drop = document.querySelector(".dropdown");
-const language = document.querySelector(".dropdown-lang");
+// const drop = document.querySelector(".dropdown");
+// const language = document.querySelector(".dropdown-lang");
 
-drop.addEventListener("click", open);
-function open(event) {
-  event.stopPropagation();
-  language.classList.toggle("languages");
+// drop.addEventListener("click", open);
+// function open(event) {
+//   event.stopPropagation();
+//   language.classList.toggle("languages");
+// }
+
+// function close() {
+//   
+
+//--------------- dropdown------------------------
+
+const dropdowns = document.querySelector(".dropdown");
+
+const select = document.querySelector(".select");
+const options = document.querySelector(".options");
+const selected = document.querySelector(".selected");
+const option = document.querySelector(".option");
+const caret = document.querySelector(".caret");
+
+function clicked(element) {
+  const value = element.innerHTML;
+  document.getElementById("selected").innerHTML = value;
+  close(options,"options--active")
+  close(caret,"caret-rotate")
 }
 
-document.addEventListener("click", close);
-function close() {
-  if (language.classList.contains("languages")) {
-    language.classList.remove("languages");
+
+select.addEventListener("click", options_open);
+  
+
+function close(cls,closed){
+  if (cls.classList.contains(closed)) {
+    cls.classList.remove(closed);
   }
 }
+function options_open(event) {
+  event.stopPropagation();
+  options.classList.toggle("options--active");
+  caret.classList.toggle("caret-rotate");
+}
 
+document.addEventListener("click", ()=>{
+  close(options,"options--active");
+  close(caret,"caret-rotate");
+});
